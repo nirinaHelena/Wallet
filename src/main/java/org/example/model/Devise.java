600bd0a8c539a1e9f04806b30f424ab73bda5a44
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-04 21:26:30 +0300

    add model device

--- src/main/java/org/example/model/Devise.java
+++ src/main/java/org/example/model/Devise.java
@@ -1,4 +1,19 @@
 package org.example.model;
 
+<<<<<<< HEAD:src/main/java/org/example/model/Devise.java
 public class Devise {
+=======
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Device {
+    private String device;
+    private String country;
+>>>>>>> 08c0a42 (add model device):src/main/java/org/example/model/Device.java
 }
