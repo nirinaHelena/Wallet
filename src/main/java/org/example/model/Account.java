19f0588d285a14d7d6a441348683d90a589dba69
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-04 22:23:48 +0300

    add constructor with id

--- src/main/java/org/example/model/Account.java
+++ src/main/java/org/example/model/Account.java
@@ -8,11 +8,18 @@ import lombok.*;
 @Setter
 @ToString
 public class Account {
-    private Integer id;
+    private int id;
     private String name;
     private String email;
+    private Double balance;
 
     public Account(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Account(int id, String name, String email) {
+        this.id = id;
         this.name = name;
         this.email = email;
     }
