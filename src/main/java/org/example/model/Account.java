c2533b82188fc12e8108966851dab116859acbed
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-04 22:16:33 +0300

    update model account

--- src/main/java/org/example/model/Account.java
+++ src/main/java/org/example/model/Account.java
@@ -8,6 +8,12 @@ import lombok.*;
 @Setter
 @ToString
 public class Account {
+    private Integer id;
     private String name;
     private String email;
+
+    public Account(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
