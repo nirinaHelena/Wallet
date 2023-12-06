c73ef1ea581e08caa37db81fd25195f01a6118c6
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 14:30:09 +0300

    update database: create table currency

--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -1,8 +1,12 @@
 -- create database wallet
 create database if not exist wallet;
 
--- TODO: create table device
 -- TODO: create table currency
+create table if not exist currency(
+    currency_id serial primary key,
+    currency_name varchar(50) not null,
+    currency_code varchar(50) not null unique
+);
 -- TODO: create table transaction
 -- TODO: create table amount
 -- TODO: create table account
