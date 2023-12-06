66f9d3dedbe5349f3119a5a61985d642e3f167e5
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 20:41:25 +0300

    update database: fix bug

--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -1,31 +1,34 @@
 -- create database wallet
-create database if not exist wallet;
+CREATE database wallet;
+
+-- connect to the database wallet
+\c wallet
 
 -- create table currency
-create table if not exist currency(
+CREATE TABLE IF NOT EXISTS currency(
     currency_id serial primary key,
     currency_name varchar(50) not null,
     currency_code varchar(50) not null unique
 );
 -- create table transaction
-create table if not exist 'transaction'(
+CREATE TABLE IF not EXISTS "transaction"(
     transaction_id serial primary key,
+    account_id int references account(account_id),
     transaction_label varchar,
-    transaction_amount double not null,
+    transaction_amount double precision not null,
     transaction_date_hour timestamp,
     transaction_type varchar not null check( transaction_type = "debit" or "credit" )
 );
 -- create table amount
-create table if not exist amount(
-    amount double not null,
+CREATE TABLE IF not EXISTS amount(
+    account_id int references account(account_id),
+    amount double precision not null,
     datetime timestamp
-)
--- create table account check amount type column, transaction list
-create table if not exist account(
+);
+-- table account
+CREATE TABLE IF NOT EXISTS account(
     account_id serial primary key,
     account_name varchar not null check(account_name ilike "courant" or "epargne"),
-    amount,
-    transaction_list,
     account_currency int references currency(currency_id),
     account_type varchar not null check (account_type ilike "banque" or "espece" or "mobile money")
 );
