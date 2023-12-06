b5612e255ea077b9f81031a759abeb69d44ddbd7
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 12:56:12 +0300

    add model device

--- src/main/java/org/example/model/Currency.java
+++ src/main/java/org/example/model/Currency.java
@@ -1,2 +1,24 @@
-package org.example.model;public class Currency {
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Currency {
+    private UUID currencyId;
+    private String currencyName;
+    private String currencyCode;
+
+    // constructor without id
+    public Currency(String currencyName, String currencyCode) {
+        this.currencyName = currencyName;
+        this.currencyCode = currencyCode;
+    }
 }
