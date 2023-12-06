33e95ca874e63953a6e9d7cfb26a866d359936e6
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 18:08:22 +0300

    create interface for create, update and save

--- src/main/java/org/example/DAO/DAOInterface.java
+++ src/main/java/org/example/DAO/DAOInterface.java
@@ -1,3 +1,9 @@
 package org.example.DAO;
 
-public interface DAOInterface {
+import java.util.List;
+
+public interface DAOInterface<T> {
+    List<T> findAll();
+    T save(T toSave);
+    List<T> saveAll(List<T> toSave);
+}
