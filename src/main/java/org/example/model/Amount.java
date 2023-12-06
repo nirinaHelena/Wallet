82493d84f85a259f70f89d33c98b9394fa4d7a6d
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 13:48:04 +0300

    add model amount

--- src/main/java/org/example/model/Amount.java
+++ src/main/java/org/example/model/Amount.java
@@ -1,2 +1,21 @@
-package org.example.model;public class Amount {
+package org.example.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Amount {
+    private double amount;
+    private LocalDateTime dateTime;
+
+    public Amount(double amount) {
+        this.amount = amount;
+        this.dateTime = LocalDateTime.now();
+    }
 }
--- src/main/resources/ScriptsDatabase/TableDevice.sql
+++
@@ -1,5 +0,0 @@
-create table  if not exist device(
-    id serial primary key,
-    device varchar not null,
-    country varchar not null
-);
