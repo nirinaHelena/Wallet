89f9e3d78814ca29e30afb92678bfed4e9360a92
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 13:54:52 +0300

    add script pqsl to create database

---
+++ src/main/resources/db.migration.database/Database.sql
@@ -0,0 +1,8 @@
+-- create database wallet
+create database if not exist wallet;
+
+-- TODO: create table device
+-- TODO: create table currency
+-- TODO: create table transaction
+-- TODO: create table amount
+-- TODO: create table account
