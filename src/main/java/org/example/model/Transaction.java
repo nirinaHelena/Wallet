55491ffe7d6b1029d71859476bac7e50a70df7c4
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 20:42:29 +0300

    add an account connected with a transaction

--- src/main/java/org/example/model/Transaction.java
+++ src/main/java/org/example/model/Transaction.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 @EqualsAndHashCode
 public class Transaction {
     private UUID transactionId;
+    private UUID account_id;
     private String transactionLabel;
     private double amount;
     private LocalDateTime dateTime;
@@ -21,8 +22,9 @@ public class Transaction {
 
     // constructor without id
 
-    public Transaction(String transactionLabel, double amount,
+    public Transaction(UUID account_id, String transactionLabel, double amount,
                        LocalDateTime dateTime, String transactionType) {
+        this.account_id= account_id;
         this.transactionLabel = transactionLabel;
         this.amount = amount;
         this.dateTime = dateTime;
