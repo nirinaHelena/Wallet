b8b71d439bb100774d3243abb0c90e1955116198
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 13:48:29 +0300

    add model account

--- src/main/java/org/example/model/Account.java
+++ src/main/java/org/example/model/Account.java
@@ -1,4 +1,35 @@
 package org.example.model;
 
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Account {
+    private UUID accountId;
+    private String accountName;
+    private Amount amount;
+    private List<Transaction> transactionList;
+    private Device device;
+    private String accountType;
+
+    // amount value initial: 0.0
+    Amount amountInitial= new Amount(0.0);
+    // constructor without accountId and amount initial= 0.0
+
+    public Account(String accountName, Device device, String accountType) {
+        this.accountName = accountName;
+        this.amount = amountInitial;
+        this.transactionList = new ArrayList<>();
+        this.device = device;
+        this.accountType = accountType;
+    }
 }
