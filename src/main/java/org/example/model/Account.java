3a0081a6e971499024bead9fcdb6c58a141315a3
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 14:29:24 +0300

    fix type device

--- src/main/java/org/example/model/Account.java
+++ src/main/java/org/example/model/Account.java
@@ -18,18 +18,18 @@ public class Account {
     private String accountName;
     private Amount amount;
     private List<Transaction> transactionList;
-    private Device device;
+    private Currency currency;
     private String accountType;
 
     // amount value initial: 0.0
     Amount amountInitial= new Amount(0.0);
     // constructor without accountId and amount initial= 0.0
 
-    public Account(String accountName, Device device, String accountType) {
+    public Account(String accountName, Currency currency, String accountType) {
         this.accountName = accountName;
         this.amount = amountInitial;
         this.transactionList = new ArrayList<>();
-        this.device = device;
+        this.currency = currency;
         this.accountType = accountType;
     }
 }
