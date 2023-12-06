cb987cef7fc5ca8f5812859a4475e4fe4799e80b
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-06 12:54:07 +0300

    add model transaction

--- src/main/java/org/example/model/Transaction.java
+++ src/main/java/org/example/model/Transaction.java
@@ -1,2 +1,31 @@
-package org.example.model;public class Transaction {
+package org.example.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Transaction {
+    private UUID transactionId;
+    private String transactionLabel;
+    private double amount;
+    private LocalDateTime dateTime;
+    private String transactionType;
+    // type: debit or credit
+
+    // constructor without id
+
+    public Transaction(String transactionLabel, double amount,
+                       LocalDateTime dateTime, String transactionType) {
+        this.transactionLabel = transactionLabel;
+        this.amount = amount;
+        this.dateTime = dateTime;
+        this.transactionType = transactionType;
+    }
 }
