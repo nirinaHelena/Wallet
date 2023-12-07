987392b8ed4e3c80165c1adf79a0c510f6a2b5e0
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-07 09:00:34 +0300

    fix: update database

--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -1,34 +1,37 @@
 -- create database wallet
-CREATE database wallet;
+CREATE DATABASE wallet;
 
 -- connect to the database wallet
-\c wallet
+\c wallet;
 
 -- create table currency
-CREATE TABLE IF NOT EXISTS currency(
-    currency_id serial primary key,
-    currency_name varchar(50) not null,
-    currency_code varchar(50) not null unique
+CREATE TABLE IF NOT EXISTS currency (
+    currency_id serial PRIMARY KEY,
+    currency_name varchar(50) NOT NULL,
+    currency_code varchar(50) NOT NULL UNIQUE
 );
+
+-- create table account
+CREATE TABLE IF NOT EXISTS account (
+    account_id serial PRIMARY KEY,
+    account_name varchar NOT NULL CHECK(account_name ILIKE 'courant' OR account_name ILIKE 'epargne'),
+    account_currency int REFERENCES currency(currency_id),
+    account_type varchar NOT NULL CHECK(account_type ILIKE 'banque' OR account_type ILIKE 'espece' OR account_type ILIKE 'mobile money')
+);
+
 -- create table transaction
-CREATE TABLE IF not EXISTS "transaction"(
-    transaction_id serial primary key,
-    account_id int references account(account_id),
+CREATE TABLE IF NOT EXISTS transaction (
+    transaction_id serial PRIMARY KEY,
+    account_id int REFERENCES account(account_id),
     transaction_label varchar,
-    transaction_amount double precision not null,
+    transaction_amount double precision NOT NULL,
     transaction_date_hour timestamp,
-    transaction_type varchar not null check( transaction_type = "debit" or "credit" )
+    transaction_type varchar NOT NULL CHECK(transaction_type IN ('debit', 'credit'))
 );
+
 -- create table amount
-CREATE TABLE IF not EXISTS amount(
-    account_id int references account(account_id),
-    amount double precision not null,
+CREATE TABLE IF NOT EXISTS amount (
+    account_id int REFERENCES account(account_id),
+    amount double precision NOT NULL,
     datetime timestamp
-);
--- table account
-CREATE TABLE IF NOT EXISTS account(
-    account_id serial primary key,
-    account_name varchar not null check(account_name ilike "courant" or "epargne"),
-    account_currency int references currency(currency_id),
-    account_type varchar not null check (account_type ilike "banque" or "espece" or "mobile money")
 );
