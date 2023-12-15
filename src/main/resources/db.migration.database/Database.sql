b3cc2ed837cb179fb1d0e19c829b4d78ad32ff7d
Sullivan1301 <jorosullivan13@gmail.com>
2023-12-15 16:35:45 +0300

    feat: move script to CreateTableTransactionCategory

--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -6,12 +6,6 @@
 
 
 
---create table transaction_category
-CREATE TABLE IF NOT EXISTS transaction_category (
-    category_id serial PRIMARY KEY,
-    category_name varchar(50) UNIQUE NOT NULL,
-    category_type varchar(50) NOT NULL CHECK(category_type IN ('debit', 'credit')),
-);
 
 -- update table transaction to add catégorie
 ALTER TABLE transaction
