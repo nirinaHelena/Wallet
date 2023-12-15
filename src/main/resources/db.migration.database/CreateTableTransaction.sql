7c41a8346281ea366541917c8606b631dffee173
Sullivan1301 <jorosullivan13@gmail.com>
2023-12-15 16:32:55 +0300

    feat: move script to CreateTableTransaction

---
+++ src/main/resources/db.migration.database/CreateTableTransaction.sql
@@ -0,0 +1,9 @@
+-- create table transaction
+CREATE TABLE IF NOT EXISTS transaction (
+    transaction_id serial PRIMARY KEY,
+    account_id int REFERENCES account(account_id),
+    transaction_label varchar,
+    transaction_amount double precision NOT NULL,
+    transaction_date_hour timestamp,
+    transaction_type varchar NOT NULL CHECK(transaction_type IN ('debit', 'credit'))
+);
--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -2,15 +2,7 @@
 
 
 -- TODO: update table transaction as "transaction"
--- create table transaction
-CREATE TABLE IF NOT EXISTS transaction (
-    transaction_id serial PRIMARY KEY,
-    account_id int REFERENCES account(account_id),
-    transaction_label varchar,
-    transaction_amount double precision NOT NULL,
-    transaction_date_hour timestamp,
-    transaction_type varchar NOT NULL CHECK(transaction_type IN ('debit', 'credit'))
-);
+
 
 -- create table amount
 CREATE TABLE IF NOT EXISTS amount (
