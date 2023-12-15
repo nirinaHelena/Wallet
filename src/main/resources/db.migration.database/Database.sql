9c7c389c967a0d907c7cc41943d3ad4a9afdf2e3
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2023-12-15 13:27:26 +0300

    fix: -- update CHECK constraint on transaction_type in transaction

--- src/main/resources/db.migration.database/Database.sql
+++ src/main/resources/db.migration.database/Database.sql
@@ -57,3 +57,15 @@ ADD CONSTRAINT unique_category_name UNIQUE (category_name);
 -- add NOT NULL constraint on category_id in transaction
 ALTER TABLE transaction
 ALTER COLUMN category_id SET NOT NULL;
+
+-- update CHECK constraint on transaction_type in transaction
+ALTER TABLE transaction
+DROP CONSTRAINT IF EXISTS valid_transaction_type;
+
+ALTER TABLE transaction
+ADD CONSTRAINT valid_transaction_type
+CHECK (
+    (transaction_type = 'debit' AND category_id IS NOT NULL AND EXISTS (SELECT 1 FROM transaction_category WHERE category_id = transaction.category_id AND category_type = 'debit'))
+    OR
+    (transaction_type = 'credit' AND category_id IS NOT NULL AND EXISTS (SELECT 1 FROM transaction_category WHERE category_id = transaction.category_id AND category_type = 'credit'))
+);
