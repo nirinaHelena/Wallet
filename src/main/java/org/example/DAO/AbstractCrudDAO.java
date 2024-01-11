16cdfd620fd4634623cc88b6fc23e67007bb7c48
Sullivan1301 <jorosullivan13@gmail.com>
2024-01-11 17:39:30 +0300

    feat: create CrudDAO AbstractCrudDAO

---
+++ src/main/java/org/example/DAO/AbstractCrudDAO.java
@@ -0,0 +1,80 @@
+package org.example.DAO;
+
+import org.example.databaseConfiguration.DatabaseConnection;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class AbstractCrudDAO<T> implements CrudDAO<T> {
+    protected DatabaseConnection connection;
+
+    public AbstractCrudDAO() {
+        this.connection = new DatabaseConnection();
+    }
+
+    protected abstract T mapResultSetToEntity(ResultSet rs);
+
+    @Override
+    public List<T> findAll() {
+        List<T> entityList = new ArrayList<>();
+
+        String sql = "SELECT * FROM " + getTableName() + ";";
+
+        try (Statement statement = connection.getConnection().createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                T entity = mapResultSetToEntity(resultSet);
+                entityList.add(entity);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return entityList;
+    }
+
+    @Override
+    public T save(T toSave) {
+        String sql = "INSERT INTO " + getTableName() + " " + getInsertColumns() + " VALUES " + getInsertValues() + ";";
+
+        try (PreparedStatement preparedStatement = connection.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            setInsertParameters(preparedStatement, toSave);
+
+            int rowsAdded = preparedStatement.executeUpdate();
+
+            if (rowsAdded > 0) {
+                try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int generatedId = generatedKeys.getInt(1);
+                        return findById(generatedId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    // Méthode abstraite pour obtenir le nom de la table dans la base de données
+    protected abstract String getTableName();
+
+    // Méthode abstraite pour obtenir les colonnes à insérer dans la requête SQL INSERT
+    protected abstract String getInsertColumns();
+
+    // Méthode abstraite pour obtenir les valeurs à insérer dans la requête SQL INSERT
+    protected abstract String getInsertValues();
+
+    // Méthode abstraite pour définir les paramètres de la requête SQL INSERT
+    protected abstract void setInsertParameters(PreparedStatement preparedStatement, T toSave);
+
+    // Méthode abstraite pour obtenir un enregistrement par son ID
+    protected abstract T findById(int id);
+
+    // Méthode abstraite pour obtenir un enregistrement par un identifiant UUID
+    protected abstract T findById(UUID id);
+}
+
